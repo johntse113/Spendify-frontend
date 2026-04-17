097b922727b20b7109a751771b3cf5ac6b215060
Hannan-Javed <hannansahib@gmail.com>
2026-04-17 15:48:04 +0800

    fix router navigation for notification icon click

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -117,7 +117,7 @@ export default function Home() {
   );
 
   const handleNotificationPress = () => {
-    router.push('/screens/notifications');
+    router.push('/screens/notifications/notifications');
   };
 
   const handleInputNewRecord = () => {
